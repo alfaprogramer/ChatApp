8feeaab5e0769154f49c3f52ca7cfe7f196c9198
alfaprogramer <adipayal2@gmail.com>
2024-06-18 21:35:24 +0530

    V0.3

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,45 @@
 import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const LoginScreen = () => {
+
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <View style={{ padding:10, alignItems:"center"}}>
+      <View style={{ padding: 10, alignItems: "center" }}>
         <KeyboardAvoidingView>
-          <View style={{marginTop:80, alignItems:"centre", justifyContent:"center"}}>
-            <Text style={{fontSize:20, fontWeight:"500"}}>Login to your Account</Text>
+          <View style={{ marginTop: 80, alignItems: "centre", justifyContent: "center" }}>
+            <Text style={{ fontSize: 20, fontWeight: "500", color:"black" }}>Login to your Account</Text>
           </View>
-          <View style={{marginTop:50}}>
+          <View style={{ marginTop: 50 }}>
             <View>
-              <Text>Email:</Text>
+              <Text style={{fontSize:18, fontWeight:'600',color:"gray" }}>Email:</Text>
               <View>
-                <TextInput value={} />
+                <TextInput value={email} onChangeText={setEmail} placeholderTextColor="#BEBEBE"
+                  style={{ width: 320, marginTop: 15,
+                     borderBottomColor: "BEBEBE", 
+                     borderBottomWidth: 1, 
+                     padding: 10, 
+                     fontFamily: "GeezaPro-Bold" ,
+                     fontSize:email?15:15
+                    }}
+                    placeholder='Enter you Email'
+                />
+              </View>
+
+              <View>
+                <TextInput value={password} onChangeText={setPassword} placeholderTextColor="#BEBEBE"
+                  style={{ width: 320, marginTop: 15,
+                     borderBottomColor: "BEBEBE", 
+                     borderBottomWidth: 1, 
+                     padding: 10, 
+                     fontFamily: "GeezaPro-Bold" ,
+                     fontSize:email?15:15
+                    }}
+                    placeholder='Enter you Password'
+                />
               </View>
             </View>
           </View>

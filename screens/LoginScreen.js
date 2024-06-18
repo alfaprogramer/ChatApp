61f76174b9546900de4e919c71237a04170655e3
alfaprogramer <adipayal2@gmail.com>
2024-06-18 21:50:17 +0530

    V 0.3

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
-import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
 
   const { email, setEmail } = useState("");
   const { password, setPassword } = useState("");
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <View style={{ padding: 10, alignItems: "center" }}>
@@ -29,6 +32,8 @@ const LoginScreen = () => {
                 />
               </View>
 
+
+              <Text style={{fontSize:18, fontWeight:'600',color:"gray", marginTop:25 }}>Password:</Text>
               <View>
                 <TextInput value={password} onChangeText={setPassword} placeholderTextColor="#BEBEBE"
                   style={{ width: 320, marginTop: 15,
@@ -42,6 +47,23 @@ const LoginScreen = () => {
                 />
               </View>
             </View>
+
+            <Pressable style={{ width:200, backgroundColor:"#4A55A2", padding:15, marginTop:50, marginLeft:"auto",
+              marginRight:"auto", borderRadius:6
+            }}>
+              <Text style={{ color:"white", fontSize:16, fontWeight:"bold", textAlign:"center"}}>Login</Text>
+            </Pressable>
+
+
+            <Pressable onPress={()=>navigation.navigate("Register")}>
+              <Text style={{textAlign:"center", color:"gray", fontSize:16, margin:12}}>Don't have an Account? Sign Up</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Image  style={{ marginTop:50, justifyContent:"centre", alignItems:"center"}}
+            />
+
+            
           </View>
 
         </KeyboardAvoidingView>
